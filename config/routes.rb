43f271a3a098082fb73d6cb937115ee06ddfed8f
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-11 11:15:42 +0900

    一覧画面の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'tweets#index'
+  resources :tweets
 
-  resources :tweet, only:[:index, :create]
 end
