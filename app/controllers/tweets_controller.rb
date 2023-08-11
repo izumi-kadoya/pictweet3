e3aa3a6016661fb292d892866a476d96b262acb9
izumi-kadoya <mt.kachikachi.if.cs@gmail.com>
2023-08-11 11:46:39 +0900

    投稿、削除機能の追加

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,29 @@ class TweetsController < ApplicationController
    def index
     @tweets = Tweet.all
    end
+
+   def new
+    @tweet = Tweet.new
+   end
+
+   def create
+    Tweet.create(tweet_params)
+    redirect_to tweets_path
+   end
+ 
+   def destroy
+    tweet = Tweet.find(params[:id])
+    tweet.destroy
+    redirect_to root_path
+   end
+
+
+    private
+  
+    def tweet_params
+       params.require(:tweet).permit(:text,:image)
+    end
+
+
+
 end
